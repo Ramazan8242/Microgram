c101624a0c24e26ce0901f7a15454b673b9e1540
Ramazan Tulanbaev <ramazan.tulanbaev@bk.ru>
2021-05-13 17:53:49 +0600

    Added publication object

--- static/script.js
+++ static/script.js
@@ -6,3 +6,17 @@ let user = {
     password : '4284254',
     isAuthorised:false
 };
+
+function authorised() {
+    user.isAuthorised = !user.isAuthorised;
+}
+
+let publication = {
+    id : '1',
+    date : '13.01.21',
+    publicationImage : "1.jpeg",
+    User:user,
+    Comment:comment,
+    likes: ""
+    // Передал User к user
+};
